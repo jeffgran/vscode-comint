76b8f0146152ce457f802101417c1903d123edfc
Jeff Gran <jeffgran@Goldilocks.local>
2022-08-17 08:08:07 -0600

    refactor

--- src/outputFilterFunctions.ts
+++ src/outputFilterFunctions.ts
@@ -6,170 +6,142 @@ export type SgrSegment = {
   endIndex: number,
 };
 
+type FilterResponse = {
+  filteredBuffer: number[],
+  openSegments: SgrSegment[],
+  sgrSegments: SgrSegment[],
+  writePosition: number[],
+  killLine: boolean,
+  inCR: boolean,
+};
+
 const tokenRe = /(\x1b\[[0-9]*[ABCDEFGJKST]|\x1b\[\?25[hl]|\x1b\[(?<code>[0-9]*;?)+m|\r+\n?|.)/g;
 
 export class CursorMovement {
-  inSlashR: boolean = false;
-  killLine: boolean = false;
-  writePosition: number[] = [-1, -1];
-  nextWritePosition: number[] = [-1, -1];
+  inCR: boolean = false;
   openSgrSegments: SgrSegment[] = [];
   sgrSegments: SgrSegment[] = [];
+  writeIndex: number = 0;
   
-  applyChunk(fileData: Uint8Array, chunk: Uint8Array): Uint8Array {
-    const filteredChunk = this.filter(chunk);
-    
-    let writeAtIndex: number;
-    
-    // note, writePosition[0] is always < 0 - doesn't make sense to count from the top
-    if (this.writePosition[0] < -1 || this.writePosition[1] >= 0) {
-      if (this.writePosition[1] < 0) {
-        // find last (nth) newline where n = -1 - wp[0]
-        // write head is that + wp[1] (it's negative so this is a subtraction)
-        writeAtIndex = fileData.lastIndexOf(10) + 1; // TODO: won't work for real, need custom function, need nth last indexof
-        // n.b. we will never hit this case yet, no tests and no known cases where this would happen. can't \r back 2 lines, but maybe with SGR codes?
-      } else {
-        // find last (nth) newline where n = 0 - wp[0]
-        // write head is that + wp[1]
-        // e.g. [-1, 0]: nth newline = 0 - -1 => 1
-        // const nthLastNewlineIndex = 0 - this.writePosition[0]; // 1 for now. TODO: nth lookup?
-        writeAtIndex = fileData.lastIndexOf(10) + 1; // 10 === \n
-      }
+  write(value: Uint8Array, to: Uint8Array): Uint8Array {
+    const headroom = to.length - this.writeIndex;
+    if (headroom >= value.length) {
+      // there's enough room, just write it
+      to.set(value, this.writeIndex);
+      this.writeIndex += value.length;
+      return to;
     } else {
-      // last newline does not matter so don't waste time.
-      // this is the most common case (-1, -1)
-      // just concat the two arrays together
-      writeAtIndex = fileData.length;
+      // there's not enough room, make a bigger array and then write it
+      const ret = new Uint8Array(to.length + (value.length - headroom));
+      ret.set(to, 0);
+      ret.set(value, this.writeIndex);
+      this.writeIndex = ret.length;
+      return ret;
     }
-    
-    if (this.writePosition[1] < 0) {
-      writeAtIndex = writeAtIndex + (this.writePosition[1] + 1); // because -1 really means "at the end", not "overwrite the last character"
-    } else {
-      writeAtIndex = writeAtIndex + (this.writePosition[1]);
-    }
-    
-    this.sgrSegments.forEach(s => {
-      s.startIndex += writeAtIndex;
-      s.endIndex += writeAtIndex;
-    });
-    
-    
-    // figure out how much the array needs to be expanded by, if any
-    // then overwrite. 
-    const writeHeadroom = fileData.length - writeAtIndex;
-    
-    
-    if (writeHeadroom >= filteredChunk.length) {
-      if (writeHeadroom > filteredChunk.length) {
-        // if we won't end at the _end_ of the final data,
-        // we have to keep track of where to start from next time.
-        this.writePosition[1] += writeHeadroom - filteredChunk.length;
-      }
-
-      if (this.killLine) {
-        const diff = writeHeadroom - filteredChunk.length;
-        if (diff > 0) {}
-        const returnData = new Uint8Array(fileData.length - diff);
-        returnData.set(fileData.slice(0, fileData.length - diff), 0);
-        returnData.set(filteredChunk, writeAtIndex);
-        return returnData;
-      } else {
-        fileData.set(filteredChunk, writeAtIndex);
-        return fileData;
-      }
-    } else {
-      // new expanded buffer
-      const diff = filteredChunk.length - writeHeadroom;
-      const returnData = new Uint8Array(fileData.length + diff);
-      returnData.set(fileData, 0);
-      returnData.set(filteredChunk, writeAtIndex);
-      return returnData;
-    }
-    
   }
   
-  filter(input: Uint8Array): Uint8Array {
-    this.writePosition = this.nextWritePosition;
-    this.nextWritePosition = [-1, -1];
-    let lastNewlineIndex = -1;
-    let filteredBuffer: number[] = [];
-    let outputWriteIndex = 0;
-    this.sgrSegments = [];
-    this.killLine = false;
-    let token: RegExpExecArray | null;
-    const inputString = input.toString();
-
-
-    while((token = tokenRe.exec(inputString)) !== null) {
+  delete(from: Uint8Array, start: number, end: number): Uint8Array {
+    const ret = new Uint8Array(from.length - (end - start));
+    ret.set(from.slice(0, start+1), 0);
+    ret.set(from.slice(end+1, from.length), start+1);
+    return ret;
+  }
+  
+  processSgrCodes(token: Token) {
+    const sgrcodes = token.sgrCodes();
+    sgrcodes?.forEach(sgrcode => {
+      if (sgrcode === 0) {
+        this.openSgrSegments.forEach(s => {
+          s.endIndex = this.writeIndex;
+          this.sgrSegments.push(s);
+        });
+        this.openSgrSegments = [];
+      } else {
+        this.openSgrSegments.push({
+          code: sgrcode,
+          startIndex: this.writeIndex,
+          endIndex: this.writeIndex
+        });
+      }
+    });
+  }
+  
+  applyChunk(fileData: Uint8Array, chunk: Uint8Array): Uint8Array {
+    const chunkString = chunk.toString();
+    let match: RegExpExecArray | null;
+    
+    
+    while((match = tokenRe.exec(chunkString)) !== null) {
+      const token = new Token(match[0]);
       //console.log(`token: ${token[0]}`, Buffer.from(token[0]));
-      if (token[0] === '\b') {
-        if (outputWriteIndex > 0) {
-          outputWriteIndex = outputWriteIndex - 1;
+      if (token.isCrlfSequence()) {
+        this.inCR = false;
+        fileData = this.write(token.outputCharCodes(), fileData);
+      } else if (token.isCrSequence()) {
+        if (match.index + match[0].length === chunkString.length) {
+          this.inCR = true;
         } else {
-          this.writePosition[1] = this.writePosition[1] - 1;
+          this.writeIndex = fileData.lastIndexOf(10) + 1;
         }
-      } else if (token[0].endsWith('\n')) { // \r*\n
-        this.inSlashR = false;
-        if (outputWriteIndex === filteredBuffer.length) {
-          filteredBuffer.push(10);
-        } else {
-          filteredBuffer[outputWriteIndex] = 10;
-        }
-        outputWriteIndex = outputWriteIndex + 1;
-        lastNewlineIndex = outputWriteIndex;
-      } else if (token[0].endsWith('\r')) {
-        if (token.index + token[0].length === inputString.length) {
-          this.inSlashR = true;
-        } else if (lastNewlineIndex >= 0) {
-          outputWriteIndex = lastNewlineIndex;
-          outputWriteIndex += 1;
-        } else {
-          this.writePosition = [-1, 0];
-          outputWriteIndex = 0;
-        }
-      } else if (token[0] === '\x1b\[K') { // 'kill to end of line' code
-        filteredBuffer.splice(outputWriteIndex);
-        this.killLine = true;
-      } else if (token[0].length > 1 && token[0][0] === '\x1b' && token[0].endsWith('m')) {
-        const sgrcode = token.groups!.code;
-        if (sgrcode === '0' || sgrcode === '') {
-          this.openSgrSegments.forEach(s => {
-            s.endIndex = outputWriteIndex - 1;
-            this.sgrSegments.push(s);
-          });
-          this.openSgrSegments = [];
-        } else {
-          this.openSgrSegments.push({
-            code: parseInt(sgrcode, 10),
-            startIndex: outputWriteIndex,
-            endIndex: outputWriteIndex - 1
-          });
-        }
-        this.inSlashR = false;
-      } else if (token[0].startsWith('\x1b[')) {
+      } else if (token.isKillLine()) {
+        fileData = this.delete(fileData, this.writeIndex - 1, fileData.length - 1);
+      } else if (token.isSgrCode()) {
+        this.processSgrCodes(token);
+        this.inCR = false;
+      } else if (token.isAnsiCode()) {
         // other ANSI codes, ignore for now
       } else { // any other character
-        if (this.inSlashR) {
-          if (lastNewlineIndex >= 0) {
-            outputWriteIndex = lastNewlineIndex + 1;
-          } else {
-            this.writePosition = [-1, 0];
-            outputWriteIndex = 0;
-          }
+        if (this.inCR) {
+          this.writeIndex = fileData.lastIndexOf(10) + 1;
         }
-        if (outputWriteIndex === filteredBuffer.length) {
-          filteredBuffer.push(token[0].charCodeAt(0));
-        } else {
-          filteredBuffer[outputWriteIndex] = token[0].charCodeAt(0);
-        }
-        outputWriteIndex += 1;
-        this.inSlashR = false;
+        fileData = this.write(token.outputCharCodes(), fileData);
+        this.inCR = false;
       }
     }
+    
+    return fileData;
+  }
+  }
 
-    if (outputWriteIndex < filteredBuffer.length) {
-      this.nextWritePosition[1] -= filteredBuffer.length - outputWriteIndex;
+class Token {
+  str: string;
+  constructor(str: string) {
+    this.str = str;
+  }
+  
+  isCrlfSequence(): boolean {
+    return this.str.startsWith('\r') && this.str.endsWith('\n');
+  }
+  
+  isCrSequence(): boolean {
+    return this.str.startsWith('\r') && this.str.endsWith('\r');
+  }
+  
+  isKillLine(): boolean {
+    return this.str === '\x1b\[K';
+  }
+  
+  isAnsiCode(): boolean {
+    return this.str.startsWith('\x1b[');
+  }
+  
+  isSgrCode(): boolean {
+    return this.str.match(/^\x1b\[([0-9];?)*m/) !== null;
+  }
+  
+  sgrCodes(): number[] | null {
+    if (!this.isSgrCode()) { return null; }
+    const match = this.str.match(/^\x1b\[(.*)m/);
+    return match![1].split(';').map(s => s === '' ? 0 : parseInt(s, 10));
+  }
+  
+  outputCharCodes(): Uint8Array {
+    if (this.isCrlfSequence()) {
+      return Buffer.from('\n');
+    } else if (!this.isAnsiCode()) {
+      return Buffer.from(this.str);
+    } else {
+      return new Uint8Array(0);
     }
-    return Buffer.from(filteredBuffer);
   }
 }
--- src/test/suite/cursorMovement.test.ts
+++ src/test/suite/cursorMovement.test.ts
@@ -5,88 +5,6 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import { CursorMovement } from '../../outputFilterFunctions';
 
-suite('CursorMovement #filter', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-	
-	test('Simple /r/n at the end of the line', () => {
-		const cm = new CursorMovement();
-		const input = "foo\r\n";
-		const output = cm.filter(Buffer.from(input));
-		assert.deepEqual(output.toString(), "foo\n");
-	});
-	
-	test('dangling /r at the end, then /r/n to start next chunk', () => {
-		const cm = new CursorMovement();
-		const input1 = "Pictures\r";
-		const input2 = "\r\nPublic\r\n";
-		
-		const output1 = cm.filter(Buffer.from(input1));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, -1]);
-		assert.equal(output1.toString(), 'Pictures');
-		
-		const output2 = cm.filter(Buffer.from(input2));
-		assert.equal(cm.inSlashR, false);
-		assert.deepEqual(cm.writePosition, [-1, -1]);
-		assert.equal(output2.toString(), '\nPublic\n');
-		
-		const finaloutput = output1.toString() + output2.toString();
-		assert.deepEqual(finaloutput, "Pictures\nPublic\n");
-	});
-	
-	// head -c 3000000000 /dev/urandom | pv -s 3000000000 > /dev/null
-	test('pv', () => {
-		const cm = new CursorMovement();
-		const input1 = '  720MiB 0:00:01 [ 720MiB/s] [=======>                          ] 25% ETA 0:00:02\r';
-		const input2 = ' 1.43GiB 0:00:02 [ 739MiB/s] [================>                 ] 51% ETA 0:00:01\r';
-		const input3 = ' 2.12GiB 0:00:03 [ 714MiB/s] [========================>         ] 75% ETA 0:00:00\r';
-		const input4 = ' 2.73GiB 0:00:04 [ 624MiB/s] [===============================>  ] 97% ETA 0:00:00\r';
-		const input5 = ' 2.79GiB 0:00:04 [ 699MiB/s] [================================>] 100%            \r\r\n';
-		
-		const output1 = cm.filter(Buffer.from(input1));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, -1]);
-		assert.deepEqual(output1.toString(), '  720MiB 0:00:01 [ 720MiB/s] [=======>                          ] 25% ETA 0:00:02');
-		
-		const output2 = cm.filter(Buffer.from(input2));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, 0]);
-		assert.deepEqual(output2.toString(), ' 1.43GiB 0:00:02 [ 739MiB/s] [================>                 ] 51% ETA 0:00:01');
-		
-		const output3 = cm.filter(Buffer.from(input3));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, 0]);
-		assert.deepEqual(output3.toString(), ' 2.12GiB 0:00:03 [ 714MiB/s] [========================>         ] 75% ETA 0:00:00');
-		
-		const output4 = cm.filter(Buffer.from(input4));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, 0]);
-		assert.deepEqual(output4.toString(), ' 2.73GiB 0:00:04 [ 624MiB/s] [===============================>  ] 97% ETA 0:00:00');
-		
-		const output5 = cm.filter(Buffer.from(input5));
-		assert.equal(cm.inSlashR, false);
-		assert.deepEqual(cm.writePosition, [-1, 0]);
-		assert.deepEqual(output5.toString(), ' 2.79GiB 0:00:04 [ 699MiB/s] [================================>] 100%            \n');
-		
-		const output6 = cm.filter(Buffer.from('next prompt >'));
-		assert.equal(cm.inSlashR, false);
-		assert.deepEqual(cm.writePosition, [-1, -1]);
-		assert.deepEqual(output6.toString(), 'next prompt >');
-	});
-	
-	test('simple SGR', () => {
-		const cm = new CursorMovement();
-		const input = 'these \x1b[32mgreen\x1b[0m and \x1b[31mred\x1b[m words';
-		const output = cm.filter(Buffer.from(input));
-		//assert.deepEqual(output.toString(), 'these green and red words');
-		assert.deepEqual(cm.openSgrSegments, []);
-		assert.deepEqual(cm.sgrSegments, [
-			{ code: 32, startIndex: 6, endIndex: 10 },
-			{ code: 31, startIndex: 16, endIndex: 18 }
-		]);
-	});
-});
-
 suite('CursorMovement #applyChunk', () => {
 	test('pv', () => {
 		const cm = new CursorMovement();
@@ -97,33 +15,27 @@ suite('CursorMovement #applyChunk', () => {
 		const input5 = ' 2.79GiB 0:00:04 [ 699MiB/s] [================================>] 100%            \r\r\n';
 		
 		const output1 = cm.applyChunk(Buffer.from(''), Buffer.from(input1));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, -1]);
+		assert.equal(cm.inCR, true);
 		assert.deepEqual(Buffer.from(output1).toString(), '  720MiB 0:00:01 [ 720MiB/s] [=======>                          ] 25% ETA 0:00:02');
 		
 		const output2 = cm.applyChunk(output1, Buffer.from(input2));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, 0]);
+		assert.equal(cm.inCR, true);
 		assert.deepEqual(Buffer.from(output2).toString(), ' 1.43GiB 0:00:02 [ 739MiB/s] [================>                 ] 51% ETA 0:00:01');
 		
 		const output3 = cm.applyChunk(output2, Buffer.from(input3));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, 0]);
+		assert.equal(cm.inCR, true);
 		assert.deepEqual(Buffer.from(output3).toString(), ' 2.12GiB 0:00:03 [ 714MiB/s] [========================>         ] 75% ETA 0:00:00');
 		
 		const output4 = cm.applyChunk(output3, Buffer.from(input4));
-		assert.equal(cm.inSlashR, true);
-		assert.deepEqual(cm.writePosition, [-1, 0]);
+		assert.equal(cm.inCR, true);
 		assert.deepEqual(Buffer.from(output4).toString(), ' 2.73GiB 0:00:04 [ 624MiB/s] [===============================>  ] 97% ETA 0:00:00');
 		
 		const output5 = cm.applyChunk(output4, Buffer.from(input5));
-		assert.equal(cm.inSlashR, false);
-		assert.deepEqual(cm.writePosition, [-1, 0]);
+		assert.equal(cm.inCR, false);
 		assert.deepEqual(Buffer.from(output5).toString(), ' 2.79GiB 0:00:04 [ 699MiB/s] [================================>] 100%            \n');
 		
 		const output6 = cm.applyChunk(output5, Buffer.from('next prompt >'));
-		assert.equal(cm.inSlashR, false);
-		assert.deepEqual(cm.writePosition, [-1, -1]);
+		assert.equal(cm.inCR, false);
 		assert.deepEqual(Buffer.from(output6).toString(), ' 2.79GiB 0:00:04 [ 699MiB/s] [================================>] 100%            \nnext prompt >');
 	});
 	
@@ -134,13 +46,11 @@ suite('CursorMovement #applyChunk', () => {
 		const input2 = 'ty first!\r\n';
 		
 		const output1 = cm.applyChunk(Buffer.from(''), Buffer.from(input1));
-		assert.equal(cm.inSlashR, false);
-		assert.deepEqual(cm.nextWritePosition, [-1, -5]);
+		assert.equal(cm.inCR, false);
 		assert.deepEqual(Buffer.from(output1).toString(), 'safeword');
 		
 		const output2 = cm.applyChunk(output1, Buffer.from(input2));
-		assert.equal(cm.inSlashR, false);
-		assert.deepEqual(cm.nextWritePosition, [-1, -1]);
+		assert.equal(cm.inCR, false);
 		assert.deepEqual(Buffer.from(output2).toString(), 'safety first!\n');	
 	});
 	
